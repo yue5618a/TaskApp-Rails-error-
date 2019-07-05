c3263922c509d206d3ac391d6c92c0336a2890c2
名無しさん <sample@email.com>
2019-07-05 07:29:36 +0000

    自動追加されたルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/show'
-
-  get 'tasks/new'
-
-  get 'tasks/edit'
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
