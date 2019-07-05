8bd6c93eac5a8a8a024345af199e129128a76bfa
名無しさん <sample@email.com>
2019-07-05 18:41:35 +0000

    Decorator導入

---
+++ test/decorators/task_decorator_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class TaskDecoratorTest < ActiveSupport::TestCase
+  def setup
+    @task = Task.new.extend TaskDecorator
+  end
+
+  # test "the truth" do
+  #   assert true
+  # end
+end
