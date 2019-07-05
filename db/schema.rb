20cf303cfec0cdac7c3ace603968da380bd53546
名無しさん <sample@email.com>
2019-07-05 17:04:43 +0000

    システム管理者属性追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190705071427) do
+ActiveRecord::Schema.define(version: 20190705170235) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20190705071427) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
