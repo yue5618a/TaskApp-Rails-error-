0d44facc468bec7aa59ef7320c5d9203280837ba
名無しさん <sample@email.com>
2019-07-05 07:26:30 +0000

    Taskモデルバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { in: 5..300 }
 end
