0dee37421ce81cc525d3cbea2a2f156f7d3eb2fc
名無しさん <sample@email.com>
2019-07-05 07:28:47 +0000

    Tasksコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
+  get 'tasks/new'
+
+  get 'tasks/edit'
+
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
