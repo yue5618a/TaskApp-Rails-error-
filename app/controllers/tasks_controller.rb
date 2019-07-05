0c636e00fc1816308f374d3d057e423553c4140d
名無しさん <sample@email.com>
2019-07-05 08:00:40 +0000

    タスク新規作成アクション

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,24 @@ class TasksController < ApplicationController
     @user = User.find(params[:user_id])
     @task = Task.new
   end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
 
   def edit
   end
+  
+  private
+
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
