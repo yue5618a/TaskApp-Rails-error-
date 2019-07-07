98173f9c9ff0784538352cf5ba79c05f7223bbb2
名無しさん <sample@email.com>
2019-07-07 16:15:03 +0000

    usersコントローラービフォアアクション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,20 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  # 現ログインユーザーであるか
+  def correct_user
+    redirect_to root_url unless current_user?(@user)
+  end
+
+  # 現ログインユーザーが管理者であるか
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+  
   # @userが定義されている上で使用する
   def admin_or_correct
     unless current_user?(@user) || current_user.admin?
-      flash[:danger] = "編集権限がありません。"
+      flash[:danger] = "権限がありません。"
       redirect_to root_url
     end  
   end
