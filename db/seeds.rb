4972413403fd051dbd1cd76ec153a3b11edc6dae
EC2 Default User <ec2-user@ip-172-31-35-250.ap-northeast-1.compute.internal>
2019-07-22 04:17:54 +0000

    TaskApp-Rails-Error-Workbook

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create!( name: "管理者",
   User.create!(name: name,
                email: email,
                password: password + (n + 1).to_s,
-               password_confirmation: password + (n + 1).to_s)
+               password_confirmation: password)
 end
 
 puts "Users Created"
